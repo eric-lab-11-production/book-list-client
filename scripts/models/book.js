e8e66f04b6e3cb7c01c1d3299eedb1dbf42bcb6a
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-04-14 16:40:32 -0700

    Created the constructor and bookview

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -7,3 +7,35 @@ ENV.isProduction = window.location.protocol === 'https:';
 ENV.productionApiUrl = 'https://es-booklist.herokuapp.com';
 ENV.devlopmentApiUrl = 'localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.devlopmentApiUrl;
+
+(function(module){
+
+  function errorCallBack(err) {
+    console.error(err);
+    //module.errorView.initErrorPage(err);
+  }
+
+  function Book(bookData) {
+    Object.keys(bookData).forEach(key => this[key] = bookData[key]);
+  }
+
+  Book.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#book-list-template').text());
+    return template(this);
+  };
+
+  Book.all = [];
+
+  Book.loadAll = rows => {
+    Book.all = rows.sort((a,b) => b.title - a.title).map(book => new Book(book));
+  };
+
+  Book.fetchAll = callback =>
+    $.get(`${ENV.apiUrl}/test`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallBack);
+
+  module.Book = Book;
+
+})(app);
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -1,3 +1,19 @@
 'use strict';
 var app = app || {};
 
+(function(module){
+
+  const bookView = {};
+
+  bookView.initIndexPage = function() {
+    app.Book.all.map(book => $('.book-list').append(book.toHtml()));
+  };
+
+  module.bookView = bookView;
+
+})(app);
+
+$(function() {
+  console.log('in IIFE?');
+  app.Book.fetchAll(app.bookView.initIndexPage);
+})
